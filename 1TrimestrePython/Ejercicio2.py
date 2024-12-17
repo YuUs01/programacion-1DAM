360e78501ce9681f552716807562964b1d0264c9
Yuso <130821066+YuUs01@users.noreply.github.com>
2024-12-17 09:29:52 +0100

    Update Ejercicio2.py
    changed the names of the variables to match the PEP8 format

--- 1TrimestrePython/Ejercicio2.py
+++ 1TrimestrePython/Ejercicio2.py
@@ -1,9 +1,9 @@
 pi = 3.14
 radio=0
-areaCirculo =0
+area_circulo =0
 print("Para visualizar el área de su cículo, introduzca el radio de este")
 
 radio = float(input("Introduce el radio del círculo"))
 
-areaCirculo = (pi*radio*radio)
-print("El área del circulo es: ",areaCirculo)
+area_circulo = (pi*radio*radio)
+print("El área del circulo es: ",area_circulo)
