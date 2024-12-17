bd1fc1911284c781a887854c0f7242ad7b7e7f5a
Yuso <130821066+YuUs01@users.noreply.github.com>
2024-12-17 09:40:31 +0100

    Create Ejercicio3.py

---
+++ 1TrimestrePython/Ejercicio3.py
@@ -0,0 +1,10 @@
+perimetro_circulo=0
+radio=0
+pi=3.14
+
+print("Para visualizar el perímetro de su circulo,")
+radio=float(input("Introduzca el radio"))
+
+perimetro_circulo=(pi*2*radio)
+
+print("El perimetro de su círculo es: ",perimetro_circulo)
