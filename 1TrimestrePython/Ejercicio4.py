ce009fbab530b3aaaf1029985eebf940629b9260
Yuso <130821066+YuUs01@users.noreply.github.com>
2024-12-18 08:22:40 +0100

    Create Ejercicio4.py

---
+++ 1TrimestrePython/Ejercicio4.py
@@ -0,0 +1,18 @@
+dia=int(input("Introduzca un numero del 1 al 7 para evaluar que día de la semana es"))
+
+if dia==1:
+    print("El día introducido es lunes")
+elif dia==2:
+    print("El día introducido es martes")
+elif dia==3:
+    print("El día introducido es miércoles")
+elif dia==4:
+    print("El día introducido es jueves")
+elif dia==5:
+    print("El día introducido es viernes")
+elif dia==6:
+    print("El día introducido es sábado")
+elif dia==7:
+    print("El día introducido es domingo")
+else:
+    print("El dato introducido no es válido")
