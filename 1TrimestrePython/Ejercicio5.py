fb1fc819ef3eae53dff8ccb60f8e7f93570c4df6
Yuso <130821066+YuUs01@users.noreply.github.com>
2024-12-18 08:35:09 +0100

    Create Ejercicio5.py

---
+++ 1TrimestrePython/Ejercicio5.py
@@ -0,0 +1,9 @@
+#Este código muestra todos los números del 1 al 100 haciendo uso de un bucle sencillo de "while"
+numero=1
+
+print("A continuación, se le van a mostrar todos los números del 1 al 100")
+
+
+while numero<101:
+    print(numero)
+    numero=numero+1
