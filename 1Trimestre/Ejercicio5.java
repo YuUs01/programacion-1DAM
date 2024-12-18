fab642657cb863389e53c2325a7eb4ec23043793
Yuso <130821066+YuUs01@users.noreply.github.com>
2024-12-18 08:28:19 +0100

    Create Ejercicio5.java

---
+++ 1Trimestre/Ejercicio5.java
@@ -0,0 +1,18 @@
+//Este algoritmo, visualiza por consola todos los números del 1 al 100.
+package traduccionejercicios;
+public class Ejercicio_5 {
+
+	public static void main(String[] args) {
+
+		System.out.println("A continuación, se van a visualizar los números del 1 al 100");
+		
+		int numero;
+		numero=1;
+		System.out.println(numero);
+		
+		while (numero<100) {
+			numero++;
+			System.out.println(numero);
+		}
+	}
+}
